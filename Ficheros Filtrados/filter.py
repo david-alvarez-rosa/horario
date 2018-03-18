95265968d9a67503ded973444e22c5edd9850fa1
paualbiol <37509236+paualbiol@users.noreply.github.com>
2018-03-18 17:14:18 +0100

    +comentaris a baix de tot

--- Ficheros Filtrados/filter.py
+++ Ficheros Filtrados/filter.py
@@ -18,4 +18,22 @@ dnoini = dnoini.reset_index(drop=True)
 dsele.to_csv('sele2.csv',encoding='UTF-8',index=False)
 dini.to_csv('ini2.csv',encoding='UTF-8',index=False)
 dnoini.to_csv('noini2.csv',encoding='UTF-8',index=False)
-    
+'''
+comprovació:
+    a = 0
+    b = 3
+    for e in dnoini.CODEX:
+        if a == b:
+            break
+        else:
+            a += 1
+            print(dnoini[dnoini.CODEX == e])
+
+merge dels 3 fitxers:
+    >>> import pandas as pd
+    >>> di = pd.read_csv('ini.csv')
+    >>> dn = pd.read_csv('noini.csv')
+    >>> ds = pd.read_csv('sele.csv')
+    >>> d1 = pd.merge(di,dn, on=['CODEX','CODASS','NF'])
+    >>> d2 = pd.merge(d1,dn,how = "outer")
+'''
