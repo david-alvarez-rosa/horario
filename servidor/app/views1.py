214cfa3f3df1b323fd149107f1531d1de9f8d8fc
paualbiol <37509236+paualbiol@users.noreply.github.com>
2018-04-21 00:13:12 +0200

    no funciona el jquery

--- servidor/app/views1.py
+++ servidor/app/views1.py
@@ -1,4 +1,4 @@
-from flask import render_template, g
+from flask import render_template
 from app import app
 from flask import request
 
@@ -6,45 +6,17 @@ from flask import request
 
 #from app.scripts.calculs import sumainterval
 #from .scripts.predictor import *
-a = 'aaa'
-a2 = ''
-a3 = ''
-a4 = ''
-a5 = ''
-a6 = ''
-a7 = ''
-a8 = ''
-a9 = ''
-a10 = ''
-a11 = ''
-a12 = ''
-a13= ''
-a14 = ''
-a15 = ''
-a16 = ''
-a17 = ''
-a18 = ''
-a19 = ''
-a20 = ''
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    
-    return render_template('p1.html',
+
+    return render_template('Web.html',
                            title='Home',
                            )
-@app.route('/pagina2', methods=['POST'])
-def next1():
-    
-    return render_template('p2.html',
-                           title='pagina2',
-                           )
-
 @app.route('/notes', methods=['POST'])
 def notes():
-    a = request.form.getlist("check_list[]")
-    print(a)
     sele = request.form['input_sele']
     algebra = request.form['input_algebra']
     calcul1 = request.form['input_calcul1']
@@ -56,13 +28,8 @@ def notes():
     expre = request.form['input_expre']
     geo = request.form['input_geo']
     rend = request.form['rend']
-        
-    print(str(rend))
-    notas = [float(sele),float(algebra),float(calcul1),float(info1),float(quim1),
-             float(mec_fon),float(quim2),float(calc2),float(expre),float(geo)]
-    print(notas)
-  
-
+    
+ 
     # en el return tornem les variables per separat, però es pot tornar un dict
     # amb els valors a tornar {'id':'valor'}
     return render_template('notes.html',
@@ -78,3 +45,7 @@ def notes():
                            expre = float(expre),
                            geo=float(geo),
                            rend = str(rend))
+
+
+
+
