e35015acf86c3a9aec8890af8676d23914bcf9ec
David Álvarez Rosa <david@alvarezrosa.com>
2018-04-22 18:06:16 +0200

    Web en tres página y añadidas todas las asignaturas en castellano

--- Servidor/app/views.py
+++ Servidor/app/views.py
@@ -2,47 +2,28 @@ from flask import render_template, g
 from app import app
 from flask import request
 
-#from app.scripts import calculs as cal
-
-#from app.scripts.calculs import sumainterval
-#from .scripts.predictor import *
-a = 'aaa'
-a2 = ''
-a3 = ''
-a4 = ''
-a5 = ''
-a6 = ''
-a7 = ''
-a8 = ''
-a9 = ''
-a10 = ''
-a11 = ''
-a12 = ''
-a13= ''
-a14 = ''
-a15 = ''
-a16 = ''
-a17 = ''
-a18 = ''
-a19 = ''
-a20 = ''
-
 
 @app.route('/')
 @app.route('/index')
-def index():
-    return render_template('p1.html', title = 'Home')
+@app.route('/horario')
+def horario():
+    return render_template('horario.html', title = 'Home')
 
 
-@app.route('/pagina2')
-def next1():
-    return render_template('p2.html', title='pagina2')
+@app.route('/deseadas')
+def deseadas():
+    return render_template('deseadas.html', title='deseadas')
 
 
-@app.route('/notes', methods=['POST'])
-def notes():
+@app.route('/cursadas', methods = ['POST'])
+def cursadas():
     a = request.form.getlist("check_list[]")
     print(a)
+    return render_template('cursadas.html', title='cursadas')
+
+
+@app.route('/resultados', methods = ['POST'])
+def resultados():
     sele = request.form['input_sele']
     algebra = request.form['input_algebra']
     calcul1 = request.form['input_calcul1']
@@ -54,12 +35,12 @@ def notes():
     expre = request.form['input_expre']
     geo = request.form['input_geo']
     rend = request.form['rend']
-        
+    
     print(str(rend))
     notas = [float(sele),float(algebra),float(calcul1),float(info1),float(quim1),
              float(mec_fon),float(quim2),float(calc2),float(expre),float(geo)]
     print(notas)
-  
+    
 
     # en el return tornem les variables per separat, però es pot tornar un dict
     # amb els valors a tornar {'id':'valor'}
